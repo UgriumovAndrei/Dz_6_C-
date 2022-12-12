3479860c9fe75b5ed41bbf079a379f4b545c3b54
ANDREI <ugryumovandrej@yandex.ru>
2022-12-12 18:29:38 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2    1, -7, 567, 89, 223-> 3
+Console.WriteLine("Вводите числа с клавиатуры, программа покажет сколько из них положительные а сколько отрицательные; чтобы прервать ввод введите 00");
+int countP = 0;
+int countO = 0;
+string n = "1";
+while(n != "00")
+{
+    n =  Console.ReadLine();
+    if(Convert.ToInt32(n)>0)countP++;
+    else if(Convert.ToInt32(n)<0)countO++;
+    else if (n == "00") break;
+    else continue;    
+}
+Console.Write($" +Числа => {countP}");
+Console.Write($" -Числа => {countO}");
